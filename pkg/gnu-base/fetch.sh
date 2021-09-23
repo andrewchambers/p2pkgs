e750340801f2e320e18c5514e365cd1f07d1a824
Andrew Chambers <ac@acha.ninja>
2021-09-23 16:39:50 +1200

    Add more packages.

---
+++ pkg/gnu-base/fetch.sh
@@ -0,0 +1,13 @@
+../../bin/prefetch \
+  https://ftp.gnu.org/gnu/coreutils/coreutils-8.31.tar.xz \
+  https://ftp.gnu.org/gnu/gawk/gawk-5.0.1.tar.xz \
+  https://ftp.gnu.org/gnu/diffutils/diffutils-3.7.tar.xz \
+  https://ftp.gnu.org/pub/gnu/findutils/findutils-4.7.0.tar.xz \
+  https://ftp.gnu.org/gnu/patch/patch-2.7.tar.gz \
+  https://ftp.gnu.org/gnu/sed/sed-4.7.tar.xz \
+  https://ftp.gnu.org/gnu/grep/grep-3.3.tar.xz \
+  https://ftp.gnu.org/gnu/which/which-2.21.tar.gz \
+  https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz \
+  https://ftp.gnu.org/gnu/gzip/gzip-1.10.tar.gz \
+  https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz \
+  https://tukaani.org/xz/xz-5.2.4.tar.gz
